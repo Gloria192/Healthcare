08086165c850d96ab6a6c29ea47abcdc0d95b2db
Gloria <mutuyegloria@gmail.com>
2025-06-05 07:50:14 -0700

    refactor: update opetation summary

--- src/main/java/org/example/healthcare/clinic/Clinic.java
+++ src/main/java/org/example/healthcare/clinic/Clinic.java
@@ -1,6 +1,28 @@
 package org.example.healthcare.clinic;
 
-public class Clinic {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    private long id;
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="clinics")
+public class Clinic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+    @Column(unique = true, length = 100)
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+    private String address;
+    @Column(unique = true, length = 100)
+    private String phone;
+    private String email;
+
 }
